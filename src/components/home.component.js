739b50964b0f13fc035df28dc7849e0fd219533f
sidahmed boutaraa <ahorob57@gmail.com>
2020-04-07 05:00:03 +0200

    the why javascript section

--- src/components/home.component.js
+++ src/components/home.component.js
@@ -54,9 +54,60 @@ function Home() {
          <main>
 
            <section className="section one">
+             <div className="text">
+               <h1>Why learn JavaScript?</h1>
+               <ul>
+                 <li>it's the most popluar that mean you can find a lot of free courses</li>
+                 <li>you can build allmost anything you want with it</li>
+                 <li>it has a lot of support from it's community</li>
+                 <li>beginner frinedly compared to many other languages</li>
+                 <li>you don't need to have any degree to start making millon's of dollar</li>
+               </ul>
+               <h1>Prerequisites</h1>
+               <ul>
+                 <li>a not so powerful computer</li>
+                 <li>a network connection</li>
+                 <li>and a basic knowladge of 1 + 1</li>
+               </ul>
+               <h1>for who is this courses</h1>
+               <p>for any one interseted in learing the basics of programming from:</p>
+               <ul>
+                 <li>HTML and CSS</li>
+                 <li>basic data structure</li>
+                 <li>basic javascript & ES6</li>
+                 <li>basic algorithm scripting</li>
+                 <li>object oriented programming</li>
+                 <li>functional programming</li>
+                 <li>regular expressions</li>
+                 <li>frontend libraries & frameworks</li>
+                 <li>JSON and AJAX</li>
+                 <li>nodeJS and data bases</li>
+                 <li>restful APIs and Microservices</li>
+                 <li>Linux command & bash scripting</li>
+               </ul>
+             </div> 
+
+             <div className="image">
+               <div>
+                <span></span>
+                <p>what are waiting for to start coding!</p>
+               </div>
+               <div>
+                 <span></span>
+                 <p>the best time to code was a year ago the second best time is now</p>
+               </div>
+               <div>
+                 <span></span>
+                 <p>how do you stand out from the rest is how you learn</p>
+               </div>
+             </div>
            </section>
 
            <section className="section two">
+             
+           </section>
+
+           <section className="section three">
             <div>
               <span></span>
               <p>Web Development In 2020</p>
@@ -84,6 +135,10 @@ function Home() {
             <div>
               <span></span>
               <p>Node.js For Beginners</p>
+            </div>
+            <div>
+              <span></span>
+              <p>JavaScript for Beginners</p>
             </div>
            </section>
 
